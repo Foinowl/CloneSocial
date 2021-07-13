be973ef87a64983c54eb6d49049ecb7cadd28f6a
Evgeniy <nifpopnaf@gmail.com>
2021-07-13 23:49:29 +0400

    add right auth layout, but modal window is not added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,30 @@
 import "@babel/polyfill"
 import "./scss/index.scss"
 
-console.log("bheeeeeeee")
+
+
+
+const btns = document.querySelectorAll(".rootButton")
+
+var button;
+
+const checkMouse = (el) => {
+  return (e) => {
+
+
+    const top = el.getBoundingClientRect().top + document.body.scrollTop
+    const left = el.getBoundingClientRect().left + document.body.scrollLeft
+
+    const relX = e.pageX - left
+    const relY = e.pageY - top
+
+    const spanEl = el.getElementsByTagName("span")[0]
+    spanEl.style.top = relY + "px"
+    spanEl.style.left = relX + "px"
+  }
+
+}
+
+
+btns.forEach((el) => el.addEventListener("mousedown", checkMouse(el)))
+
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -35,7 +35,7 @@ module.exports = {
   },
   devtool: isDev ? 'source-map' : false,
   devServer: {
-    port: 3000,
+    port: 3005,
     hot: isDev
   },
   plugins: [
