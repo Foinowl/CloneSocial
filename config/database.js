fae22b616deb0f6279ee7101cfa739bfb3efd126
Evgeniy <nifpopnaf@gmail.com>
2021-07-20 21:51:54 +0400

    add option in confgif db

--- config/database.js
+++ config/database.js
@@ -23,10 +23,6 @@ module.exports = {
 	// 	},
 	// },
 	production: {
-		username: process.env.DB_USER,
-		password: process.env.DB_PASSWORD,
-		database: process.env.DB_DATABASE,
-		host: process.env.DB_HOST,
 		dialect: "postgres",
 		dialectOptions: {
 			ssl: true,
