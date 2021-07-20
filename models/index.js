c398a7874eadb4b583640d9c3abc766086453cd1
Evgeniy <nifpopnaf@gmail.com>
2021-07-20 23:24:43 +0400

    fwe

--- models/index.js
+++ models/index.js
@@ -13,7 +13,7 @@ if (config.use_env_variable) {
 	console.log("config.use_env_variable", config.use_env_variable)
 	console.log(
 		"process.env[config.use_env_variable]",
-		config.process.env[config.use_env_variable]
+		process.env[config.use_env_variable]
 	)
 	sequelize = new Sequelize(process.env[config.use_env_variable], config)
 } else {
