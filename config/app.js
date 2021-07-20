513a917d80ec03be85b7c25ee70684eff151d481
Evgeniy <nifpopnaf@gmail.com>
2021-07-20 23:44:02 +0400

    chage

--- config/app.js
+++ config/app.js
@@ -1,8 +1,8 @@
 require("dotenv").config()
 
 module.exports = {
-	appKey: process.env.NODE_ENV ? process.env.appKey : process.env.APP_KEY,
-	appUrl: process.env.NODE_ENV ? process.env.URL : process.env.APP_URL,
-	appPort: process.env.NODE_ENV ? process.env.PORT : process.env.APP_PORT,
+	appKey: process.env.NODE_ENV === 'production' ? process.env.appKey : process.env.APP_KEY,
+	appUrl: process.env.NODE_ENV === 'production' ? process.env.URL : process.env.APP_URL,
+	appPort: process.env.NODE_ENV === 'production' ? process.env.PORT : process.env.APP_PORT,
 }
 
